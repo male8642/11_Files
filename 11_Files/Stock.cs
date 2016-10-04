72d99914cc988f17661a4aeec0a6cb33d600cfcb
Malene <Malene@LAPTOP-M0S97IA3>
2016-10-04 10:59:57 +0200

    Add project files.

---
+++ 11_Files/Stock.cs
@@ -0,0 +1,16 @@
+﻿namespace _11_Files
+{
+    internal class Stock
+    {
+        private string symbol;
+        private double pricepershare;
+        private int numshares;
+
+        public Stock(string symbol, double pricepershare, int numshares)
+        {
+            this.symbol = symbol;
+            this.pricepershare = pricepershare;
+            this.numshares = numshares;
+        }
+    }
+}
